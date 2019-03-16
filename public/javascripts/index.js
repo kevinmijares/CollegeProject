345f7f6ac4d602357b23a2e4e9a0f3fd882b1f80
kevin <kevinmijares@gmail.com>
2019-03-15 20:58:42 -0600

    Adde AJAX request for API. Added button that makes AJAX request to load more posts.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,15 +11,15 @@ $(function (){
         .done(function(posts){
             var reversePosts = posts.slice()
 
-            if(postsLoaded == reversePosts.length){
+            if(postsLoaded >= reversePosts.length){
                 $("#loadButton").text("No More Posts") 
             }
             else{
-                for(i; (i <= postsLoaded+ 3) || (i == reversePosts.length -1); i++){
+                for(i; (i <= reversePosts.length - 1) && (i <= postsLoaded+ 3) ; i++){
+                    console.log(reversePosts.length)
+                    console.log(i)
 
-
-
-                    $("#postsContainer").append('<div class="col-lg-2 col-md-4 col-sm-6 padding-0 container"> <a href="/post/' +reversePosts[i]._id + '"><img src="/images/' + reversePosts[i].images[0]+ '" class="img-fluid "></a><div class="middle"><a href="/post/' +reversePosts[i]._id+'"><div class="text">' + reversePosts[i].name + '</div></a></div></div>')
+                    $("#postsContainer").append('<div class="col-lg-2 col-md-4 col-sm-6 padding-0 container> <a href="/post/' +reversePosts[i]._id + '"><img src="/images/' + reversePosts[i].images[0]+ '" class="img-fluid "></a><div class="middle"><a href="/post/' +reversePosts[i]._id+'"><div class="text">' + reversePosts[i].name + '</div></a></div></div>')
                 }
                 postsLoaded = i;
                      
@@ -39,3 +39,16 @@ $(document).scroll(function(){
     })
 })
 
+
+function isScrolledIntoView(elem){
+    var $elem = $("#contact");
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
